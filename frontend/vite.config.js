9c70e5a19502131c0fa5d6e04971be168569353c
Kai Habermann <kai.habermann@gmx.net>
2026-02-03 12:13:32 +0100

    ...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { writeFileSync } from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/LHCbDecFileViewer/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'create-nojekyll',
+      closeBundle() {
+        // Create .nojekyll file to prevent GitHub Pages from using Jekyll
+        writeFileSync('dist/.nojekyll', '')
+      }
+    }
+  ],
 })
 
